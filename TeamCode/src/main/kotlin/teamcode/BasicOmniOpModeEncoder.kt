af63ea87c81b51bf615204829be0951548b975a8
Pratyush-K-Science <Pratyushpnk@gmail.com>
2025-12-04 16:10:28 -0600

    Servo teleop changes

--- TeamCode/src/main/kotlin/teamcode/BasicOmniOpModeEncoder.kt
+++ TeamCode/src/main/kotlin/teamcode/BasicOmniOpModeEncoder.kt
@@ -1,31 +1,4 @@
-/* Copyright (c) 2021 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+/* Copyright (c) 2021 FIRST. All rights reserved. */
 package teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
@@ -37,11 +10,10 @@ import kotlin.math.abs
 import com.qualcomm.robotcore.hardware.Servo
 import kotlin.math.max
 
-
 @Suppress("unused")
 @TeleOp(name = "Basic: Omni Linear OpMode Encoder", group = "Linear OpMode")
 class BasicOmniOpModeEncoder_Linear : LinearOpMode() {
-    // Declare OpMode members for each of the 4 motors.
+
     private val runtime = ElapsedTime()
     private var frontLeftDrive: DcMotor? = null
     private var backLeftDrive: DcMotor? = null
@@ -51,8 +23,8 @@ class BasicOmniOpModeEncoder_Linear : LinearOpMode() {
     private var launchMoto: DcMotor? = null
     private var intakeRuns:DcMotor? = null
 
-    // Servo + kicker state (only part changed) -----------------------------
-    private var pusher: Servo? = null     // servo on Expansion Hub 2, port 0, name: "servo_motor"
+    // Servo + kicker state
+    private var pusher: Servo? = null
     private val REST_POS = 1.0
     private val KICK_POS = 0.5
     private val KICK_TIME_MS = 1500
@@ -60,13 +32,11 @@ class BasicOmniOpModeEncoder_Linear : LinearOpMode() {
     private enum class KickState { IDLE, EXTENDING, RETRACTING }
     private var kickState = KickState.IDLE
     private val phaseTimer = ElapsedTime()
-    // ----------------------------------------------------------------
 
     private val LAUNCHER_POWER = 1.0
     private val intake_power = 0.65
 
     override fun runOpMode() {
-        // Initialize the hardware variables.
 
         frontLeftDrive = hardwareMap.get(DcMotor::class.java, "front_left")
         backLeftDrive = hardwareMap.get(DcMotor::class.java, "back_left")
@@ -82,7 +52,6 @@ class BasicOmniOpModeEncoder_Linear : LinearOpMode() {
         frontRightDrive!!.direction = DcMotorSimple.Direction.FORWARD
         backRightDrive!!.direction = DcMotorSimple.Direction.FORWARD
 
-
         launchMoto!!.direction = DcMotorSimple.Direction.REVERSE
         intakeRuns!!.direction = DcMotorSimple.Direction.REVERSE
 
@@ -97,25 +66,22 @@ class BasicOmniOpModeEncoder_Linear : LinearOpMode() {
         backLeftDrive!!.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         backRightDrive!!.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
 
-
         var launcherEnabled = false
         var prevB = false
         var intakeEnabled = false
         var prevA = false
-        var prevX = false          // added for X button edge-detect (servo only)
+        var prevX = false
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+
             this.telemetry.addLine("frontLeft" + frontLeftDrive!!.currentPosition)
             this.telemetry.addLine("frontRight" + frontRightDrive!!.currentPosition)
             this.telemetry.addLine("backLeft" + backLeftDrive!!.currentPosition)
             this.telemetry.addLine("backRight" + backRightDrive!!.currentPosition)
 
-
             var max: Double
 
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            val axial = -gamepad1.left_stick_y.toDouble() // Note: pushing stick forward gives negative value
+            val axial = -gamepad1.left_stick_y.toDouble()
             val lateral = gamepad1.left_stick_x.toDouble()
             val yaw = gamepad1.right_stick_x.toDouble()
 
@@ -124,20 +90,18 @@ class BasicOmniOpModeEncoder_Linear : LinearOpMode() {
                 launcherEnabled = !launcherEnabled
             }
             prevB = bNow
+
             val aNow = gamepad2.a
             if(aNow && !prevA){
                 intakeEnabled = !intakeEnabled
             }
             prevA = aNow
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
+
             var frontLeftPower = axial + lateral + yaw
             var frontRightPower = axial - lateral - yaw
             var backLeftPower = axial - lateral + yaw
             var backRightPower = axial + lateral - yaw
 
-            // Normalize the values so no wheel power exceeds 100% (This basically makes it so that nothing goes above 100)
-            // This ensures that the robot maintains the desired motion.
             max = max(abs(frontLeftPower), abs(frontRightPower))
             max = max(max, abs(backLeftPower))
             max = max(max, abs(backRightPower))
@@ -149,20 +113,16 @@ class BasicOmniOpModeEncoder_Linear : LinearOpMode() {
                 backRightPower /= max
             }
 
-            // Send calculated power to wheels
             frontLeftDrive!!.power = frontLeftPower
             frontRightDrive!!.power = frontRightPower
             backLeftDrive!!.power = backLeftPower
             backRightDrive!!.power = backRightPower
 
-            // Send Power to the Launcher
             launchMoto!!.power = if(launcherEnabled) LAUNCHER_POWER else 0.0
             intakeRuns!!.power = if(intakeEnabled) intake_power else 0.0
 
-            // Pressing x makes the servo trigger and return to original position
             val xNow = gamepad2.x
 
-            // On rising edge of X and only if not in a kick, start a cycle
             if (xNow && !prevX && kickState == KickState.IDLE) {
                 pusher!!.position = KICK_POS
                 phaseTimer.reset()
@@ -170,10 +130,18 @@ class BasicOmniOpModeEncoder_Linear : LinearOpMode() {
             }
             prevX = xNow
 
+            // <<< ADDED: servo state machine for timed retract
+            when (kickState) {
+                KickState.EXTENDING -> {
+                    if (phaseTimer.milliseconds() >= KICK_TIME_MS.toDouble()) {
+                        pusher!!.position = REST_POS       // <<< ADDED
+                        kickState = KickState.IDLE          // <<< ADDED
+                    }
+                }
+                else -> {}
+            }
+            // <<< END ADDED SECTION
 
-            // ---------------------------------------------------------
-
-            // Telemetry gives all of the data about the hardware, etc along with power
             telemetry.addData("Info", "Run Time: $runtime")
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower)
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower)
