aa926a2d1e61240cf7217c209f923331caf662b8
Pratyush-K-Science <Pratyushpnk@gmail.com>
2025-12-05 17:22:26 -0600

    Changed import setting on the code.

--- TeamCode/src/main/kotlin/teamcode/RobotAutoDriveToAprilTagOmni.kt
+++ TeamCode/src/main/kotlin/teamcode/RobotAutoDriveToAprilTagOmni.kt
@@ -28,7 +28,6 @@
  */
 package teamcode
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
